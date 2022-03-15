58d8d02953f9b71cc997a97b36cb11be88cc8963
stokatlio2012 <stokatlio2012@gmail.com>
2022-03-16 01:41:36 +0300

    Merhaba Dünya ödevi yapıldı. Semih

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,5 +1,5 @@
 public class FirstHw {
     public static void main(String[] args) {
-        //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
+        System.out.println("Merhaba Dünya!");
     }
 }
